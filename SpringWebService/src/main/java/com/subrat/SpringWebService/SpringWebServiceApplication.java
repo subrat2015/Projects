bd5b576affc7ea4eb8de68b8537d9eb006753890
Subrat Parida <subrat2015@gmail.com>
2019-10-16 18:51:36 -0700

    Spring web service project

---
+++ SpringWebService/src/main/java/com/subrat/SpringWebService/SpringWebServiceApplication.java
@@ -0,0 +1,26 @@
+package com.subrat.SpringWebService;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+
+@SpringBootApplication
+public class SpringWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+}
